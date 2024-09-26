8b5aab105580b0edf41037b8363c03c6c8ded56e
KpuBemKa <kakoitopis@gmail.com>
2024-09-26 12:38:49 +0300

    text encoding bug fix

--- settings.py
+++ settings.py
@@ -9,6 +9,6 @@ LOG_FORMAT = "%(asctime)s - %(levelname)s - %(module)s - %(message)s"
 # the folder in which data from esps is stored
 RECORDINGS_FOLDER = "esp-recordings"
 
-DELETE_CONVERTED_FILES = False
+DELETE_CONVERTED_FILES = True
 
 ALLOWED_EXTENSIONS = [".wav", ".mp3", ".ogg"]
