638350b223a0e7caa8dcae9849f16022d1b3c300
KpuBemKa <kakoitopis@gmail.com>
2025-01-13 18:20:02 +0400

    message

--- modules/bot_replies.py
+++ modules/bot_replies.py
@@ -1,11 +1,13 @@
 START_REPLY = (
     "Hello! I am Reviewer ⭐️ and I am offering discounts and rewards for your honest opinion! 🗣️\n\n"
-    "Just send me a Voice Note 🎙 with your comments what you liked or did not like about this place and get a guaranteed reward 🏆\n\n"
+    "Just send me a Voice Note 🎙 with your comments what did you like or did not like about this place and get a guaranteed reward 🏆\n\n"
     "Press the microphone and speak in your native language 💬"
 )
 START_ATTACHEMENT_PATH = "static/start-attachement.jpg"
 
-ATTACHEMENT_DENIED = "Sorry, I can accept only Voice Notes"
-FILETYPE_DENIED = "Sorry, I can accept only Voice Notes"
+ATTACHEMENT_DENIED = "Sorry, I can accept only Voice Notes 🎙"
+FILETYPE_DENIED = "Sorry, I can accept only Voice Notes 🎙"
 
-FILE_ACCEPTED = "Thank you for your feedback! We will work harder to provide a better service"
+FILE_ACCEPTED = (
+    "Thank you for your feedback! ❤️ \n\nWe will work harder to provide a better service 🫶🏼"
+)
